b73dbe010f17a5b53768aafa760e1e1bcda76c42
530454062@qq.com <vacuum158013>
2020-03-01 11:34:37 +0800

    first commit

--- src/main/java/com/spring/config/MainConfig2.java
+++ src/main/java/com/spring/config/MainConfig2.java
@@ -9,9 +9,9 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class MainConfig2 {
     /**
-     * @Scope
-     * prototype:多实例的
-     * singleton:单实例的
+     * @Scope 调整作用域
+     * prototype:多实例的:ioc容器启动不会调用创建对象的方法，每次获取的时候才会调用创建对象的方法，并且每次获取对象都会调用。
+     * singleton:单实例的（默认值）：ioc容器启动就会调用创建对象的方法，将对象放进IOC容器中，以后使用就是直接从容器(map.get())取到。
      * request:同一个请求创建一个实例(WEB环境下)
      * session:同一个session创建一个实例(WEB环境下)
      */
@@ -19,6 +19,7 @@ public class MainConfig2 {
     //默认是单实例的
     @Bean("scopeperson")
     public Person person() {
+        System.out.println("bean正在创建......");
         return new Person("张三", 20);
     }
 }
